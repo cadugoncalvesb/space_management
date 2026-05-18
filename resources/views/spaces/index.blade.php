61371d404b17eb5878161645b7714686d69fdad2
Carlos Eduardo Goncalves Braga <cadubraga680@gmail.com>
2026-05-18 18:56:27 -0300

    fix(space): correct the status of the space

--- resources/views/spaces/index.blade.php
+++ resources/views/spaces/index.blade.php
@@ -38,84 +38,91 @@
                         <div class="overflow-x-auto bg-white rounded-lg border border-gray-200 shadow-sm mt-4">
                             <table class="w-full divide-y divide-gray-200">
                                 <thead class="bg-gray-50">
-                                    <tr>
+                                <tr>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Nome do espaço
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Local (Prédio)
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Tipo
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Capacidade
+                                    </th>
+                                    <th scope="col"
+                                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Status
+                                    </th>
+                                    @if(auth()->user()->role === 'admin')
                                         <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Nome do espaço
+                                            class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Ações
                                         </th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Local (Prédio)
-                                        </th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Tipo
-                                        </th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Capacidade
-                                        </th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Status
-                                        </th>
-                                        @if(auth()->user()->role === 'admin')
-                                            <th scope="col"
-                                                class="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                Ações
-                                            </th>
-                                        @endif
-                                    </tr>
+                                    @endif
+                                </tr>
                                 </thead>
 
                                 <tbody class="bg-white divide-y divide-gray-200">
-                                    @foreach($spaces as $space)
-                                        <tr class="hover:bg-gray-50 transition duration-150">
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <div class="text-sm font-medium text-gray-900">{{ $space->name }}</div>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <div
-                                                    class="text-sm font-medium text-gray-900">{{ $space->local->name }}</div>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <div
-                                                    class="text-sm font-medium text-gray-900 text-center">{{ $space->type }}</div>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <div
-                                                    class="text-sm font-medium text-gray-900 text-center">{{ $space->capacity }}</div>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <div
-                                                    class="text-sm font-medium text-gray-900 text-center">{{ $space->status }}</div>
-                                            </td>
+                                @foreach($spaces as $space)
+                                    <tr class="hover:bg-gray-50 transition duration-150">
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm font-medium text-gray-900">{{ $space->name }}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div
+                                                class="text-sm font-medium text-gray-900">{{ $space->local->name }}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div
+                                                class="text-sm font-medium text-gray-900 text-center">{{ $space->type }}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div
+                                                class="text-sm font-medium text-gray-900 text-center">{{ $space->capacity }}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div
+                                                class="text-sm font-medium text-gray-900 text-center">
+                                                {{ match ($space->status) {
+                                                    'active' => 'Ativo',
+                                                    'inactive' => 'Inativo',
+                                                    'maintenance' => 'Em manutenção',
+                                                    default => 'Desconhecido'
+                                                } }}
+                                            </div>
+                                        </td>
 
-                                            @if(auth()->user()->role === 'admin')
-                                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                    <div class="flex items-center justify-end gap-3">
-                                                        <a href="{{ route('spaces.edit', $space->id) }}"
-                                                           class="inline-flex items-center px-3 py-1.5 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150">
-                                                            Editar
-                                                        </a>
+                                        @if(auth()->user()->role === 'admin')
+                                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                <div class="flex items-center justify-end gap-3">
+                                                    <a href="{{ route('spaces.edit', $space->id) }}"
+                                                       class="inline-flex items-center px-3 py-1.5 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150">
+                                                        Editar
+                                                    </a>
 
-                                                        <form action="{{ route('spaces.destroy', $space->id) }}"
-                                                              method="POST"
-                                                              style="display:inline;"
-                                                              onsubmit="return confirm('Tem certeza que deseja exluir este espaço?')">
-                                                            {{--                                            @if({{$space-}}) @endif--}}
-                                                            @csrf
-                                                            @method('DELETE')
-                                                            <button type="submit"
-                                                                    class="inline-flex items-center px-3 py-1.5 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                                                                Excluir
-                                                            </button>
-                                                        </form>
-                                                    </div>
-                                                </td>
-                                            @endif
-                                        </tr>
-                                    @endforeach
+                                                    <form action="{{ route('spaces.destroy', $space->id) }}"
+                                                          method="POST"
+                                                          style="display:inline;"
+                                                          onsubmit="return confirm('Tem certeza que deseja exluir este espaço?')">
+                                                        {{--                                            @if({{$space-}}) @endif--}}
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit"
+                                                                class="inline-flex items-center px-3 py-1.5 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                                                            Excluir
+                                                        </button>
+                                                    </form>
+                                                </div>
+                                            </td>
+                                        @endif
+                                    </tr>
+                                @endforeach
                                 </tbody>
                             </table>
                         </div>
